dae7e6f856777cd907ae88c13d59fb8f9391028c
yongqianvip <yongqainvip@163.com>
2017-12-04 18:10:13 +0800

    add timerActiveTitle

--- CountDownButton.js
+++ CountDownButton.js
@@ -33,11 +33,13 @@ export default class CountDownButton extends React.Component {
 	  disableColor: PropTypes.string,
 	  timerTitle: PropTypes.string,
 	  enable: PropTypes.oneOfType([PropTypes.bool,PropTypes.number]),
-	  timerEnd: PropTypes.func
+	  timerEnd: PropTypes.func,
+	  timerActiveTitle: PropTypes.array
 	};
 
 	_countDownAction(){
 		const codeTime = this.state.timerCount;
+		const {timerActiveTitle, timerTitle} = this.props
 		const now = Date.now()
 		const overTimeStamp = now + codeTime * 1000 + 100/*过期时间戳（毫秒） +100 毫秒容错*/
 		this.interval = setInterval(() =>{
@@ -48,7 +50,7 @@ export default class CountDownButton extends React.Component {
 				this.interval&&clearInterval(this.interval);
 				this.setState({
 					timerCount: codeTime,
-					timerTitle: this.props.timerTitle || '获取验证码',
+					timerTitle: timerTitle || '获取验证码',
 					counting: false,
 					selfEnable: true
 				})
@@ -57,9 +59,17 @@ export default class CountDownButton extends React.Component {
 				};
 			}else{
 				const leftTime = parseInt((overTimeStamp - nowStamp)/1000, 10)
+				let activeTitle = `重新获取(${leftTime}s)`
+				if (timerActiveTitle) {
+					if (timerActiveTitle.length > 0) {
+						activeTitle = timerActiveTitle[0] + leftTime
+					}else if (timerActiveTitle.length > 1) {
+						activeTitle = timerActiveTitle[0] + leftTime + timerActiveTitle[1]
+					}
+				}
 				this.setState({
 					timerCount: leftTime,
-					timerTitle: `重新获取(${leftTime}s)`,
+					timerTitle: activeTitle,
 				})
 			}
 			/* 切换到后台 timer 停止计时 */
