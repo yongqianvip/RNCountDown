9d8a477c8703867c5e92466a0010c584592cab9b
yinyongqian <yinyongqian@renrenche.com>
2019-03-18 18:28:13 +0800

    better style

--- src/CountDownButton.js
+++ src/CountDownButton.js
@@ -16,7 +16,7 @@ import {
 } from 'react-native';
 export default class CountDownButton extends React.Component {
 	constructor(props) {
-	  super(props)
+		super(props)
 		this.state = {
 			timerCount: this.props.timerCount || 60,
 			timerTitle: this.props.timerTitle || '获取验证码',
@@ -27,28 +27,28 @@ export default class CountDownButton extends React.Component {
 		this._countDownAction = this._countDownAction.bind(this)
 	}
 	static propTypes = {
-	  style: ViewPropTypes.style,
-	  textStyle: Text.propTypes.style,
-	  onClick: PropTypes.func,
-	  disableColor: PropTypes.string,
-	  timerTitle: PropTypes.string,
-	  enable: PropTypes.oneOfType([PropTypes.bool,PropTypes.number]),
-	  timerEnd: PropTypes.func,
+		style: ViewPropTypes.style,
+		textStyle: Text.propTypes.style,
+		onClick: PropTypes.func,
+		disableColor: PropTypes.string,
+		timerTitle: PropTypes.string,
+		enable: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+		timerEnd: PropTypes.func,
 		timerActiveTitle: PropTypes.array,
 		executeFunc: PropTypes.func
 	};
 
-	_countDownAction(){
+	_countDownAction() {
 		const codeTime = this.state.timerCount;
-		const {timerActiveTitle, timerTitle} = this.props
+		const { timerActiveTitle, timerTitle } = this.props
 		const now = Date.now()
 		const overTimeStamp = now + codeTime * 1000 + 100/*过期时间戳（毫秒） +100 毫秒容错*/
-		this.interval = setInterval(() =>{
+		this.interval = setInterval(() => {
 			/* 切换到后台不受影响*/
 			const nowStamp = Date.now()
 			if (nowStamp >= overTimeStamp) {
 				/* 倒计时结束*/
-				this.interval&&clearInterval(this.interval);
+				this.interval && clearInterval(this.interval);
 				this.setState({
 					timerCount: codeTime,
 					timerTitle: timerTitle || '获取验证码',
@@ -58,13 +58,13 @@ export default class CountDownButton extends React.Component {
 				if (this.props.timerEnd) {
 					this.props.timerEnd()
 				};
-			}else{
-				const leftTime = parseInt((overTimeStamp - nowStamp)/1000, 10)
+			} else {
+				const leftTime = parseInt((overTimeStamp - nowStamp) / 1000, 10)
 				let activeTitle = `重新获取(${leftTime}s)`
 				if (timerActiveTitle) {
 					if (timerActiveTitle.length > 1) {
 						activeTitle = timerActiveTitle[0] + leftTime + timerActiveTitle[1]
-					}else if (timerActiveTitle.length > 0) {
+					} else if (timerActiveTitle.length > 0) {
 						activeTitle = timerActiveTitle[0] + leftTime
 					}
 				}
@@ -91,37 +91,39 @@ export default class CountDownButton extends React.Component {
 				})
 			}
 			*/
-		},1000)
+		}, 1000)
 	}
-	_shouldStartCountting(shouldStart){
-		if (this.state.counting) {return}
+	_shouldStartCountting(shouldStart) {
+		if (this.state.counting) { return }
 		if (shouldStart) {
 			this._countDownAction()
-			this.setState({counting: true,selfEnable:false})
-		}else{
-			this.setState({selfEnable:true})
+			this.setState({ counting: true, selfEnable: false })
+		} else {
+			this.setState({ selfEnable: true })
 		}
 	}
 	componentDidMount() {
 		const { executeFunc } = this.props;
 		executeFunc && executeFunc(this._shouldStartCountting);
 	}
-	componentWillUnmount(){
+	componentWillUnmount() {
 		clearInterval(this.interval)
 	}
-	render(){
-		const {onClick,style,textStyle,enable,disableColor} = this.props
-		const {counting,timerTitle,selfEnable} = this.state
+	render() {
+		const { onClick, style, textStyle, enable, disableColor } = this.props
+		const { counting, timerTitle, selfEnable } = this.state
 		return (
-			<TouchableOpacity activeOpacity={counting ? 1 : 0.8} onPress={()=>{
-				if (!counting && enable && selfEnable) {
-					this.setState({selfEnable:false})
-					onClick(this._shouldStartCountting)
-				};
-			}}>
-				<View style={[{width:120, height:44,justifyContent:'center',alignItems:'center'},style]}>
-					<Text style={[{fontSize: 16},textStyle,{color: ((!counting && enable && selfEnable) ? (textStyle ? textStyle.color : 'blue') : disableColor || 'gray')}]}>{timerTitle}</Text>
-				</View>
+			<TouchableOpacity
+				activeOpacity={counting ? 1 : 0.8}
+				onPress={() => {
+					if (!counting && enable && selfEnable) {
+						this.setState({ selfEnable: false })
+						onClick(this._shouldStartCountting)
+					};
+				}}
+				style={[{ width: 120, height: 44, justifyContent: 'center', alignItems: 'center' }, style]}
+			>
+				<Text style={[{ fontSize: 16 }, textStyle, { color: ((!counting && enable && selfEnable) ? (textStyle ? textStyle.color : 'blue') : disableColor || 'gray') }]}>{timerTitle}</Text>
 			</TouchableOpacity>
 		)
 	}
